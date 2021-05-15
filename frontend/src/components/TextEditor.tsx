50d0aac724838bb0bbc105c59fd82c6b5dffc32d
Black-Tomcat <tomcat.frantz@gmail.com>
2021-05-15 19:30:45 +1000

    Update frontend with Material ui and slate
    add fonts

---
+++ frontend/src/components/TextEditor.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode, useCallback, useMemo, useState } from "react";
+import { Editable, ReactEditor, Slate, withReact } from "slate-react";
+import { BaseEditor, createEditor, Editor } from "slate";
+import { Card, Typography } from "@material-ui/core";
+
+type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomText = { text: string };
+
+declare module "slate" {
+    interface CustomTypes {
+        Editor: BaseEditor & ReactEditor;
+        Element: CustomElement;
+        Text: CustomText;
+    }
+}
+
+const TextEditor = () => {
+    const editor = useMemo<ReactEditor>(() => withReact(createEditor()), []);
+    const renderElement = useCallback(({ attributes, children, element }) => {
+        switch (element.type) {
+            default:
+                return (
+                    <Typography variant={"body1"} {...attributes}>
+                        {children}
+                    </Typography>
+                );
+        }
+    }, []);
+    const [value, setValue] = useState([
+        {
+            type: "paragraph" as "paragraph",
+            children: [{ text: "A line of text in a paragraph." }],
+        },
+    ]);
+    return (
+        // Add the editable component inside the context.
+        <Card style={{margin: 8, padding: 8}}>
+            <Slate
+                editor={editor}
+                value={value}
+                // @ts-ignore
+                onChange={(newValue) => setValue(newValue)}
+            >
+                <Editable renderElement={renderElement} />
+            </Slate>
+        </Card>
+    );
+};
+
+export default TextEditor;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
+import "@fontsource/julius-sans-one";
+import "@fontsource/athiti";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {
+    createMuiTheme, CssBaseline,
+    responsiveFontSizes,
+    ThemeProvider
+} from "@material-ui/core";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider
+            theme={responsiveFontSizes(
+                createMuiTheme({
+                    palette: {
+                        type: "dark",
+                        primary: { main: "#2e7d32" },
+                        secondary: { main: "#b71c1c" },
+                    },
+                    typography: {
+                        h1: { fontFamily: "Julius Sans One" },
+                        h2: { fontFamily: "Julius Sans One" },
+                        h3: { fontFamily: "Julius Sans One" },
+                        h4: { fontFamily: "Julius Sans One" },
+                        h5: { fontFamily: "Julius Sans One" },
+                        h6: { fontFamily: "Julius Sans One" },
+                        subtitle1: { fontFamily: "Julius Sans One" },
+                        subtitle2: { fontFamily: "Julius Sans One" },
+                        body1: { fontFamily: "Athiti" },
+                        body2: { fontFamily: "Athiti" },
+                        button: { fontFamily: "Athiti" },
+                        caption: { fontFamily: "Athiti" },
+                        overline: { fontFamily: "Athiti" },
+                    },
+                })
+            )}
+        >
+            <CssBaseline/>
+            <App />
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById("root")
 );
