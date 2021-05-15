2fc89c86dde205a28069e97932ec60fa07c457c8
Black-Tomcat <tomcat.frantz@gmail.com>
2021-05-15 15:38:48 +1000

    initial commit

---
+++ frontend/craco.config.ts
@@ -0,0 +1,59 @@
+const {
+    createWebpackDevConfig,
+    createWebpackProdConfig,
+    when,
+    whenDev,
+    whenProd,
+    whenTest,
+    ESLINT_MODES,
+    POSTCSS_MODES,
+} = require("@craco/craco");
+
+let flagged = false;
+
+module.exports = {
+    plugins: [
+        {
+            plugin: {
+                overrideWebpackConfig: ({
+                    webpackConfig,
+                    cracoConfig,
+                    pluginOptions,
+                    context: { env, paths },
+                }) => {
+                    if (flagged) {
+                        console.log(paths);
+                        return webpackConfig;
+                    }
+                    flagged = true;
+                    // console.log(JSON.stringify(webpackConfig, null, 4));
+
+                    // console.log(paths);
+                    [
+                        "appPath",
+                        "appBuild",
+                        "appPublic",
+                        "appHtml",
+                        "appIndexJs",
+                        "appSrc",
+                        "testsSetup",
+                        "proxySetup",
+                        "swSrc",
+                        "appTypeDeclarations",
+                    ].forEach(
+                        (item) =>
+                            (paths[item] = paths[item].replace(
+                                /([\\/])blerpsblerg/g,
+                                "$1blerpsblerg$1frontend"
+                            ))
+                    );
+                    paths.appIndexJs = paths.appIndexJs.replace(".js", ".tsx")
+                    console.log(paths);
+
+                    // Always return the config object.
+                    return createWebpackDevConfig(cracoConfig, { env, paths });
+                },
+            },
+        },
+    ],
+};
